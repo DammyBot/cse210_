7a7a5b60d108395857c616d820a5600a741b0500
bot_fx <97201812+DammyBot@users.noreply.github.com>
2025-11-12 14:17:59 +0100

    Learning activity w03

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction(1);
+        Fraction fraction2 = new Fraction(5);
+        Fraction fraction3 = new Fraction(3,4);
+        Fraction fraction4 = new Fraction(1,3);
+        Console.WriteLine($"Fraction 1: {fraction1.GetFractionString()} = {fraction1.GetDecimalValue()}");
+        Console.WriteLine($"Fraction 2: {fraction2.GetFractionString()} = {fraction2.GetDecimalValue()}");
+        Console.WriteLine($"Fraction 3: {fraction3.GetFractionString()} = {fraction3.GetDecimalValue()}");
+        Console.WriteLine($"Fraction 4: {fraction4.GetFractionString()} = {fraction4.GetDecimalValue()}");
     }
 }
