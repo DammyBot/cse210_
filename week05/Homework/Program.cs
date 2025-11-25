2a0a938d007d73b493c7261f9bcd2a6a743fa46a
bot_fx <97201812+DammyBot@users.noreply.github.com>
2025-11-25 11:19:51 +0100

    W05 Learning Activity: Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Dammy", "Game Development");
+        // Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("7.3", "1-10", "Dammy", "Fractions");
+        // Console.WriteLine(mathAssignment.GetSummary());
+        // Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("The Causes of World War II", "Dammy", "History");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
